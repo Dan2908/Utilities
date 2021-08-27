08de69cb95fbe79370f610dbfc9dc4b6cf86fa2a
Dan2908 <danilobrandolin29@gmail.com>
2021-08-27 15:31:20 -0300

    Added some array utilities

--- testbox.cpp
+++ testbox.cpp
@@ -3,45 +3,23 @@
 #include <chrono>
 #include "utilities.h"
 
-template < class dataType >
-  void printArray(util::dynamic_array < dataType > * p_array) {
-    printf("PRINT VALUES, SIZE: %i\n|", p_array -> size());
-    for (int i = 0; i < p_array -> size(); i++) {
-      printf("%i\t|", p_array -> get(i));
+template <class dataType>
+void printArray(util::dynamic_array<dataType> *p_array)
+{
+    printf("PRINT VALUES, SIZE: %i\n|", p_array->size());
+    for (int i = 0; i < p_array->size(); i++)
+    {
+        printf("%i\t|", p_array->get(i));
     }
     printf("\n\n");
-  }
-
-typedef util::dynamic_array < int > intArray;
-
-template<class T, class... Pp >
-void test_function(T( * F)(Pp...), Pp... p_args) {
-  auto start = std::chrono::high_resolution_clock::now();
-  F(p_args...);
-  auto end = std::chrono::high_resolution_clock::now();
-  printf("it took %i ticks, or %f seconds.\n", start - end, (float)end / CLOCKS_PER_SEC);
 }
 
-int main() {
-  int arraySize = 3000;
+typedef util::dynamic_array<int> intArray;
 
-  intArray from(arraySize);
-  intArray to(arraySize);
-
-  for (int i = 0; i < arraySize; i++) {
-    from.get(i) = (i * 2) + 1;
-    to.get(i) = (i * 2);
-  }
-  
-  printf("%li \n", CLOCKS_PER_SEC);
+int main()
+{
+    int arraySize = 40000;
 
 
-  auto start = std::chrono::high_resolution_clock::now();
-  util::duff_copy(&from.get(0), &to.get(0), arraySize);
-  auto end = std::chrono::high_resolution_clock::now();
-  printf("it took %i ticks, or %f seconds.\n", end - start, 0);
-
-
-  //printArray( & to);
-  return 0;
+    return 0;
 }
--- utilities.h
+++ utilities.h
@@ -1,37 +1,67 @@
 #ifndef MY_ARRAYS_H
 #define MY_ARRAYS_H
 #include <cstdarg>
-#include <stdio.h>
 
-#define F_MIN(A,B) ( (A < B) ? A : B )
+#define F_MIN(A, B) ((A < B) ? A : B)
 
-namespace util{
+namespace util
+{
     const unsigned C_BIG_ARRAY = 512; /* Minimum size of data in bytes to consider an array a big array */
 
     /** Copies [p_count] data from [p_orig] to [p_dest] using Duff's Device.*/
     void duff_copy(int *p_orig, int *p_dest, int p_count);
-    /** Copies [p_count] data from [p_orig] to [p_dest].*/
+    /** Copies [p_count] data from [p_orig] to [p_dest] using for loop.*/
     void copy_array(int *p_orig, int *p_dest, int p_count);
+    /** Measures the execution time of function [p_function] in nanoseconds.
+    * This functions implements std::chrono libraries to do the task.*/
+    template <class T, class... Ts>
+    int function_duration(T (*p_function)(Ts...), Ts... p_args)
+    {
+        using namespace std::chrono;
+        steady_clock::time_point begin, end;
+        begin = steady_clock::now();
+        p_function(p_args...);
+        end = steady_clock::now();
 
-    template<class T>
-    class dynamic_array{
+        return duration_cast<nanoseconds>(end - begin).count();
+    }
+    
+    /** Compares the first [p_size] elements of two arrays.
+     * @return  true if contents in both arrays are equal, false otherwise.*/
+    template <class T>
+    bool compare_arrays(T *p_leftArray, T *p_rightArray, size_t p_size)
+    {
+        for (int i = 0; i < p_size; i++)
+            if (p_leftArray[i] != p_rightArray[i]) return false;
+        return true;
+    }
+
+    template <class T>
+    class dynamic_array
+    {
         T *m_array;
         int m_size;
     public:
         dynamic_array(int p_size) : m_size(p_size), m_array(new T[p_size]) {}
-        ~dynamic_array(){
-            if(m_array != 0) delete[] m_array;
+        ~dynamic_array()
+        {
+            if (m_array != 0)
+                delete[] m_array;
             m_array = 0;
         }
+        bool operator==(dynamic_array<T>& p_other)  { return compare_arrays(m_array, p_other, m_size); }
+
         /*  Adds cells to the array. If [p_end] is true, cells are added at the end of the array,
            or at the beginning otherwise.*/
-        void add_cells(int p_count, bool p_end){
+        void add_cells(int p_count, bool p_end)
+        {
             //  Create new empty array of new size //
             T *newarray = new T[m_size + p_count];
             //  Check allocating error  //
-            if(newarray == 0) return;
+            if (newarray == 0)
+                return;
             //  Copy old data   //
-            int offset = (p_end) ? 0 : p_count; 
+            int offset = (p_end) ? 0 : p_count;
             duff_copy(m_array, &newarray[offset], m_size);
             //  Delete old array
             delete[] m_array;
@@ -41,13 +71,15 @@ namespace util{
         }
         /*  Removes cells from the array. If [p_end] is true, end cells are removed. Otherwise 
             first cells are removed. */
-        void rem_cells(int p_count, bool p_end){
+        void rem_cells(int p_count, bool p_end)
+        {
             //  Check if p_count exeeds array size  //
-            if(p_count > m_size) return;
+            if (p_count > m_size)
+                return;
             //  Delete cells    //
             T *toRemove = (p_end) ? &m_array[m_size - p_count] : m_array;
             m_array = (!p_end) ? &m_array[p_count - 1] : m_array;
-            
+
             printf("ptr value -> %i\n", toRemove);
             realloc(toRemove, sizeof(T) * p_count);
             printf("ptr value -> %i\n", toRemove);
@@ -57,100 +89,123 @@ namespace util{
             m_size -= p_count;
         }
         /* Returns areference to the value in the array at p_index */
-        T& get( int p_index ){
+        T &get(int p_index)
+        {
             return m_array[p_index];
         }
         /* Returns array size */
-        int size(){
+        int size()
+        {
             return m_size;
         }
+        T &operator[](int p_index) { return m_array[p_index]; }
+        (*operator T()) { return m_array; }
         /* Set all array values to [p_value]. Fills with zero if not specified*/
-        void set( int p_value = 0){
-
+        void set(int p_value = 0)
+        {
         }
     };
 };
 #else
 
-    /** struct for reserving dimensional size data for multi-dimensional arrays*/
-    struct MultiDimSizing{
-        MultiDimSizing(int p_nDimension, int p_sizes...){
-            int auxarray[p_nDimension] = {p_sizes};
-            m_sizes = auxarray;
-            m_nDimension = p_nDimension;
-        }
-        ~MultiDimSizing(){
-            if(m_sizes != 0)
-                delete[] m_sizes;
-            m_sizes = 0;
-        }
-        int& operator[](int p_index){
-            return m_sizes[p_index];
-        }
-        int& GetDimension(){
-            return m_nDimension;
-        }
-        void SetSize(int p_dest, int p_new_size){
-            m_sizes[p_dest] = p_new_size;
-        }
-    private:
-        int m_nDimension;
-        int *m_sizes;    
-    };
-    
+/** struct for reserving dimensional size data for multi-dimensional arrays*/
+struct MultiDimSizing
+{
+    MultiDimSizing(int p_nDimension, int p_sizes...)
+    {
+        int auxarray[p_nDimension] = {p_sizes};
+        m_sizes = auxarray;
+        m_nDimension = p_nDimension;
+    }
+    ~MultiDimSizing()
+    {
+        if (m_sizes != 0)
+            delete[] m_sizes;
+        m_sizes = 0;
+    }
+    int &operator[](int p_index)
+    {
+        return m_sizes[p_index];
+    }
+    int &GetDimension()
+    {
+        return m_nDimension;
+    }
+    void SetSize(int p_dest, int p_new_size)
+    {
+        m_sizes[p_dest] = p_new_size;
+    }
+
+private:
+    int m_nDimension;
+    int *m_sizes;
 };
+}
+;
 
 /**
  * Array<class> template for arrays of n dimensions.
  */
-template<class dataType>
-class Array{
+template <class dataType>
+class Array
+{
     dataType *m_array;
     util::MultiDimSizing m_sizes;
 
-    int  m_getGridCount(const int n, int *p_sizes) const{
-        if(n < 0) return 1;
-        return p_sizes[n] * m_getGridCount(n-1, p_sizes);
+    int m_getGridCount(const int n, int *p_sizes) const
+    {
+        if (n < 0)
+            return 1;
+        return p_sizes[n] * m_getGridCount(n - 1, p_sizes);
     }
-    int  m_getGridPosition( int *p_params, const int n) const{
-        if(n == 0) return p_params[0];
-        return p_params[n] * m_getGridCount(n-1, m_sizes) + m_getGridPosition(p_params, n-1);
+    int m_getGridPosition(int *p_params, const int n) const
+    {
+        if (n == 0)
+            return p_params[0];
+        return p_params[n] * m_getGridCount(n - 1, m_sizes) + m_getGridPosition(p_params, n - 1);
     }
     /* Compares two n-Dimensional arrays and returns a n-Dimensional array with the minimum values of each*/
-    int* m_getMinArray( int *p_array, int *p_other, int n){
+    int *m_getMinArray(int *p_array, int *p_other, int n)
+    {
         int *minarray = new int[n];
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             minarray[i] = F_MIN(p_array[i], p_other[i]);
         return minarray;
     }
 
 public:
-/** Creates an array of dataType with n dimensions. Dimensions are calculated by the number of parameters
+    /** Creates an array of dataType with n dimensions. Dimensions are calculated by the number of parameters
  * passed to it's constructor.
  * example: Array<int> arr(3x3x3) builds a zero initialized 3D array. 
  * @param[in] p_dimensions comma-separated sizes, each parameter refers to each dimension. 3, 2, ..., n gives
  * a n-Dimension array.
  */
-    Array( const int p_dimensions... ){
+    Array(const int p_dimensions...)
+    {
         /* Get total array size and dimensions array (width, height, depth, ...) */
         int array_size = 1;
-        m_sizes = new int(p_dimensions);      // Contains dimension sizes W, H, D, ...
-        m_dimensionN = static_cast<int>(sizeof(m_sizes)/sizeof(int));
-        for(int i = 0; i < m_dimensionN; i++){
-            array_size *= m_sizes[i];                // Calculate m_array size
+        m_sizes = new int(p_dimensions); // Contains dimension sizes W, H, D, ...
+        m_dimensionN = static_cast<int>(sizeof(m_sizes) / sizeof(int));
+        for (int i = 0; i < m_dimensionN; i++)
+        {
+            array_size *= m_sizes[i]; // Calculate m_array size
         }
         /* Create new array */
         m_array = new dataType[array_size];
     }
-    ~Array(){
-        if(m_array != 0) delete[] m_array;
-        if(m_sizes != 0) delete[] m_sizes;
+    ~Array()
+    {
+        if (m_array != 0)
+            delete[] m_array;
+        if (m_sizes != 0)
+            delete[] m_sizes;
         m_array = 0;
         m_sizes = 0;
     }
-    dataType& Get( const int p_position... ){
+    dataType &Get(const int p_position...)
+    {
         int parameters[] = {p_position};
-        return m_array[ (m_getGridPosition(parameters, m_dimensionN - 1 ))];
+        return m_array[(m_getGridPosition(parameters, m_dimensionN - 1))];
     }
     /**
      * Resizes the array into the current dimension bound.
@@ -159,22 +214,23 @@ public:
      * Example: Array<int> arr(3, 3, 3); arr.Resize(3, 4, 5, 6, 7) will give an array
      * of 3x4x5, while sizes 6 and 7 are ignored.
     */
-    template<typename... Ts>
-    void Resize( Ts... p_sizes ){
-        if(sizeof...(p_sizes) < m_dimensionN)
+    template <typename... Ts>
+    void Resize(Ts... p_sizes)
+    {
+        if (sizeof...(p_sizes) < m_dimensionN)
             return;
         //New data array;
         dataType *newarray = 0;
         //Save new sizes
         int newsizes[] = {p_sizes...};
         //Allocate new array
-        newarray = new dataType[ m_getGridCount( m_dimensionN - 1, newsizes) ];
-        if(newarray == 0) return;                                     //Catch Allocation error
+        newarray = new dataType[m_getGridCount(m_dimensionN - 1, newsizes)];
+        if (newarray == 0)
+            return; //Catch Allocation error
 
-        int *min = m_getMinArray( m_sizes, newsizes);
+        int *min = m_getMinArray(m_sizes, newsizes);
 
         //TODO: copy old array dato to the new
-
     }
 };
 #endif //MY_ARRAYS_H
